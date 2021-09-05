7bf254e0252e7514316bfe2c4d4fbc475ff90457
Ramya Sree <56461863+ramyasree19@users.noreply.github.com>
2021-09-05 09:34:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import 'react-native-gesture-handler';
 import { Image,FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View,Button } from "react-native";
 import * as React from 'react';
-//import {View, TouchableOpacity, Image} from 'react-native';
-
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-//import {Ionicons} from "@expo/vector-icons";
 import Home from './components/HomePage';
 import CustomerApp from './components/BookDetails';
 import Login from './components/Login';
@@ -20,29 +17,6 @@ import CustomSidebarMenu from './CustomSideBarMenu';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
-
-const NavigationDrawerStructure = (props) => {
-  //Structure for the navigatin Drawer
-  const toggleDrawer = () => {
-    //Props to open/close the drawer
-    props.navigationProps.toggleDrawer();
-  };
-
-  return (
-    <View style={{flexDirection: 'row'}}>
-      <TouchableOpacity onPress={toggleDrawer}>
-        {/*Donute Button Image */}
-        <Image
-          source={{
-            uri:
-            'https://cdn.w600.comps.canstockphoto.com/three-lines-blue-circle-icon-clip-art-vector_csp50754261.jpg',
-          }}
-          style={styles.tinyLogo}
-        />
-      </TouchableOpacity>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   container: {
